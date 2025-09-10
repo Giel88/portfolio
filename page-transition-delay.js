5327af3204d81c733d011190674d27e96d4034e1
Giel88 <giel88@gmail.com>
2025-09-10 13:41:27 +0200

    Create page-transition-delay.js

---
+++ page-transition-delay.js
@@ -0,0 +1,18 @@
+// © Code by T.RICKS, https://www.tricksdesign.com/
+// You have the license to use this code in your projects but not redistribute it to others
+
+// Works on internal links, ignores external links or links containing #
+function internalLink(myLink) {
+    return (myLink.host == window.location.host);
+}
+$('a').each(function() {
+  if (internalLink(this) && (this).href.indexOf('#') === -1) {
+  $(this).click(function(e){
+  e.preventDefault();
+  var moduleURL = jQuery(this).attr("href");
+  setTimeout( function() { window.location = moduleURL }, 700 );
+  // Class that has page out interaction tied to click
+  $('.page-transition').click();
+});
+    }
+});
