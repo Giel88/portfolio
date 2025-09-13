b8cefb4b449e48e308bc638152b2b51b699a100c
Giel88 <giel88@gmail.com>
2025-09-13 11:10:20 +0200

    Update hover.js

--- hover.js
+++ hover.js
@@ -1,4 +1,4 @@
-// Pak alle case-links
+// CASE LIST
 document.querySelectorAll('.case-link').forEach(link => {
   // vind de linktitle binnen dit specifieke link-item
   const title = link.querySelector('.linktitle');
@@ -11,6 +11,23 @@ document.querySelectorAll('.case-link').forEach(link => {
 
   // hover out
   link.addEventListener('mouseleave', () => {
+    gsap.to(title, { x: 0, duration: 0.3, ease: "power2.out" });
+  });
+});
+
+// BUTTON
+document.querySelectorAll('.button').forEach(btn => {
+  // vind de linktitle binnen deze button
+  const title = btn.querySelector('.linktitle');
+  if (!title) return;
+
+  // hover in
+  btn.addEventListener('mouseenter', () => {
+    gsap.to(title, { x: 20, duration: 0.3, ease: "power2.out" });
+  });
+
+  // hover out
+  btn.addEventListener('mouseleave', () => {
     gsap.to(title, { x: 0, duration: 0.3, ease: "power2.out" });
   });
 });
