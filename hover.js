641163529b8a7c9257b75ee09393d97d6abada34
Giel88 <giel88@gmail.com>
2025-09-13 11:30:57 +0200

    Update hover.js

--- hover.js
+++ hover.js
@@ -1,51 +1,54 @@
-// CASE LIST
-document.querySelectorAll('.case-link').forEach(link => {
-  const title = link.querySelector('.linktitle');
-  if (!title) return;
+if (window.matchMedia("(min-width: 992px)").matches) {
 
-  // hover in
-  link.addEventListener('mouseenter', () => {
-    gsap.to(title, { 
-      x: 20,
-      duration: 0.5,
-      ease: "back.out(2)",
-      overwrite: "auto"
+  // CASE LIST
+  document.querySelectorAll('.case-link').forEach(link => {
+    const title = link.querySelector('.linktitle');
+    if (!title) return;
+  
+    // hover in
+    link.addEventListener('mouseenter', () => {
+      gsap.to(title, { 
+        x: 20,
+        duration: 0.5,
+        ease: "back.out(2)",
+        overwrite: "auto"
+      });
+    });
+  
+    // hover out
+    link.addEventListener('mouseleave', () => {
+      gsap.to(title, { 
+        x: 0,
+        duration: 0.3,
+        ease: "bounce.out",
+        overwrite: "auto"
+      });
     });
   });
-
-  // hover out
-  link.addEventListener('mouseleave', () => {
-    gsap.to(title, { 
-      x: 0,
-      duration: 0.3,
-      ease: "bounce.out",
-      overwrite: "auto"
+  
+  // BUTTON
+  document.querySelectorAll('.button').forEach(btn => {
+    const title = btn.querySelector('.linktitle');
+    if (!title) return;
+  
+    // hover in
+    btn.addEventListener('mouseenter', () => {
+      gsap.to(title, { 
+        x: 20,
+        duration: 0.5,
+        ease: "back.out(2)",
+        overwrite: "auto"
+      });
+    });
+  
+    // hover out
+    btn.addEventListener('mouseleave', () => {
+      gsap.to(title, { 
+        x: 0,
+        duration: 0.3,
+        ease: "bounce.out",
+        overwrite: "auto"
+      });
     });
   });
-});
-
-// BUTTON
-document.querySelectorAll('.button').forEach(btn => {
-  const title = btn.querySelector('.linktitle');
-  if (!title) return;
-
-  // hover in
-  btn.addEventListener('mouseenter', () => {
-    gsap.to(title, { 
-      x: 20,
-      duration: 0.5,
-      ease: "back.out(2)",
-      overwrite: "auto"
-    });
-  });
-
-  // hover out
-  btn.addEventListener('mouseleave', () => {
-    gsap.to(title, { 
-      x: 0,
-      duration: 0.3,
-      ease: "bounce.out",
-      overwrite: "auto"
-    });
-  });
-});
+}
