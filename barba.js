7574475c2ecbb324732770b14aba758e678d9130
Giel88 <giel88@gmail.com>
2025-09-14 20:16:12 +0200

    Update barba.js

--- barba.js
+++ barba.js
@@ -1,23 +1,28 @@
 barba.init({
-    transitions: [
-      {
+    transitions: [{
         name: 'default',
         async leave(data) {
-          console.log('leave');
-          console.log(data);
-          await gsap.to(data.current.container, { opacity: 0, duration: 1 });
-        },
+            console.log('leave');
+            console.log(data);
+            await gsap.to(data.current.container, { opacity: 0, duration: 1 });
+            },
+            
         async enter(data) {
-          console.log('enter');
-          console.log(data);
-          window.scrollTo(0, 0); 
-          gsap.set(data.next.container, { opacity: 0 });              
-        },     
+            console.log('enter');
+            console.log(data);
+            window.scrollTo(0, 0); 
+            gsap.set(data.next.container, { opacity: 0 });
+            if (window.Webflow && window.Webflow.require) {
+                const ix2 = window.Webflow.require('ix2');
+                ix2.init(data.next.container);
+                }            
+            },  
+            
         async afterEnter(data) {
-          console.log('afterEnter');
-          console.log(data);      
-          await gsap.to(data.next.container, { opacity: 1, duration: 1 });                 
-        },                      
-      }
-      ]
+            console.log('afterEnter');
+            console.log(data);      
+            await gsap.to(data.next.container, { opacity: 1, duration: 1 });                 
+            },                      
+        }
+    ]
 });
