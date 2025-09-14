8d52ff87f7e882db66ed5650f09cb3c3ddc5474f
Giel88 <giel88@gmail.com>
2025-09-14 22:00:55 +0200

    Update barba.js

--- barba.js
+++ barba.js
@@ -1,38 +1,15 @@
 barba.init({
   transitions: [
     {
-      name: 'default',
-
-      // Oude container fade-out
-      async leave({ current, next }) {
-        console.log('leave', current);     
-        await gsap.to(current.container, { autoAlpha: 0, duration: 1 });
-      },
-
-      // Enter hook: scroll reset
-      enter({ next }) {
-        console.log('enter', next);
-        window.scrollTo(0, 0);
-      },
-
-      // AfterEnter: nieuwe container fade-in + Webflow IX2 init
-      afterEnter({ next }) {
-        console.log('afterEnter', next);
-
-        // Forceer container onzichtbaar
-        gsap.set(next.container, { autoAlpha: 0 });
-
-        // Kleine timeout zodat browser DOM painted voordat animatie start
-        setTimeout(() => {
-          gsap.to(next.container, { autoAlpha: 1, duration: 1 });
-
-          // Webflow IX2 reset na fade
-          if (window.Webflow && window.Webflow.require) {
-            const ix2 = window.Webflow.require('ix2');
-            ix2.init(next.container);
-          }
-        }, 50); // 50ms is meestal voldoende
-      },
-    },
-  ],
+      name: 'color-only',
+      leave({ next }) {
+        const newColor = next.container.dataset.themeColor;
+        return gsap.to("body", {
+          backgroundColor: newColor,
+          duration: 1,
+          ease: "power2.inOut"
+        });
+      }
+    }
+  ]
 });
