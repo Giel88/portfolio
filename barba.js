3040823ca98d97c3c791f2a394d1fe2d505e4522
Giel88 <giel88@gmail.com>
2025-09-14 22:25:32 +0200

    Update barba.js

--- barba.js
+++ barba.js
@@ -2,9 +2,9 @@ barba.init({
   transitions: [
     {
       name: 'color-fade',
-      leave({ next }) {
-        // pak de kleur direct van de link waarop geklikt is
-        const newColor = next.trigger.dataset.color;
+      leave({ current, next }) {
+        // fallback: gebruik kleur van current als next.trigger ontbreekt
+        const newColor = next.trigger?.dataset.color || current.container.dataset.themeColor;
 
         return gsap.to("body", {
           backgroundColor: newColor,
