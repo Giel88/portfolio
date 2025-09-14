f8fdc390e0ed2656eaac78f576239420c52a4487
Giel88 <giel88@gmail.com>
2025-09-14 17:01:12 +0200

    Update barba.js

--- barba.js
+++ barba.js
@@ -2,18 +2,38 @@ barba.init({
   transitions: [{
     name: 'test-leave',
 
+    // Eerste keer laden
     once({ next }) {
       // Fade in container
-      return gsap.to(next.container, { opacity: 1, duration: 1, ease: "power2.out" });
+      console.log("once triggered!");
+      return gsap.to(next.container, {
+        opacity: 1,
+        duration: 1,
+        ease: "power2.out"
+      });
     },    
-    
-    leave({ current, next }) {
+
+    // Pagina verlaten
+    leave({ current }) {
       console.log("leave triggered!");
-      return new Promise(resolve => setTimeout(resolve, 500));
+      return gsap.to(current.container, {
+        opacity: 0,
+        duration: 0.5,
+        ease: "power2.out"
+      }).then(() => {
+        // Extra delay van 0.5s voordat 'enter' start
+        return new Promise(resolve => setTimeout(resolve, 500));
+      });
     },
 
+    // Nieuwe pagina binnenkomen
     enter({ next }) {
       console.log("enter triggered!");
+      return gsap.to(next.container, {
+        opacity: 1,
+        duration: 0.5,
+        ease: "power2.out"
+      });
     }
   }]
 });
