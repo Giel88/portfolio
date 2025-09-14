1ce4a2c5a0ed1db67f966697ff46cd11b607b11b
Giel88 <giel88@gmail.com>
2025-09-14 22:16:19 +0200

    Update barba.js

--- barba.js
+++ barba.js
@@ -5,7 +5,7 @@ barba.init({
       leave({ current, next }) {
         const color = current.container.dataset.themeColor;        
         console.log("Leave gestart", current, next);
-        return gsap.to("body", { backgroundColor: "color", duration: 1 });
+        return gsap.to("body", { backgroundColor: color, duration: 1 });
       },
       enter({ next }) {
         console.log("Enter gestart", next);
