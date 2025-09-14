6f9e3ed646c806a9e34b896766fc7a87878c2d1b
Giel88 <giel88@gmail.com>
2025-09-14 16:34:51 +0200

    Update barba.js

--- barba.js
+++ barba.js
@@ -1,39 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  barba.init({
-    transitions: [{
-      name: 'fade-color-transition',
-
-      // Eerste keer laden
-      once({ next }) {
-        const color = next.container.dataset.themeColor || "var(--bg)";
-
-        // Achtergrond instellen
-        gsap.set("body", { backgroundColor: color });
-
-        // Fade in container
-        return gsap.to(next.container, { opacity: 1, duration: 1 });
-      },
-
-      // Pagina verlaten
-      leave({ current, next }) {
-        const nextColor = next.container.dataset.themeColor || "var(--bg)";
-
-        // Fade-out huidige container + achtergrond veranderen
-        console.log("leave triggered");
-        return gsap.to(current.container, {
-          opacity: 0,
-          duration: 1,
-          onUpdate: () => gsap.set("body", { backgroundColor: nextColor })
-        });
-      },
-
-      // Nieuwe pagina binnenkomen
-      enter({ next }) {
-        // Fade in nieuwe container
-        return gsap.to(next.container, { opacity: 1, duration: 1 });
-      }
-    }]
-  });
-
+ barba.init({
+  transitions: [{
+    name: 'opacity-transition',
+    leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 0
+      });
+    },
+    enter(data) {
+      return gsap.from(data.next.container, {
+        opacity: 0
+      });
+    }
+  }]
 });
