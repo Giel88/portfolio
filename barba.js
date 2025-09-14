f2e8d4b8b9b6f8464d930c680829ed2441805894
Giel88 <giel88@gmail.com>
2025-09-14 22:22:18 +0200

    Update barba.js

--- barba.js
+++ barba.js
@@ -1,14 +1,16 @@
 barba.init({
   transitions: [
     {
-      name: 'debug',
-      leave({ current, next }) {
-        const color = next.trigger.dataset.color;        
-        console.log("Leave gestart", current, next);
-        return gsap.to("body", { backgroundColor: color, duration: 1 });
-      },
-      enter({ next }) {
-        console.log("Enter gestart", next);
+      name: 'color-fade',
+      leave({ next }) {
+        // pak de kleur direct van de link waarop geklikt is
+        const newColor = next.trigger.dataset.color;
+
+        return gsap.to("body", {
+          backgroundColor: newColor,
+          duration: 0.6,
+          ease: "power2.inOut"
+        });
       }
     }
   ]
