6166a711734fbb5c1befd4ce9d1512728f03106b
Giel88 <giel88@gmail.com>
2025-09-15 15:23:53 +0200

    Update text-loop.js

--- text-loop.js
+++ text-loop.js
@@ -118,7 +118,7 @@ const container = document.querySelector(".scroll-container");
 const containerWidth = container.offsetWidth;
 
 // Snelheid berekenen op basis van breedte
-const speed = containerWidth / 4000;
+const speed = containerWidth / 6000;
 
 const tl = horizontalLoop(items, {
   repeat: -1,
