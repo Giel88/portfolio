a02e2bc0f1ef93a3bf7e12e82382d3756e527bac
Giel88 <giel88@gmail.com>
2025-09-15 23:02:13 +0200

    Update scrolltrigger.js

--- scrolltrigger.js
+++ scrolltrigger.js
@@ -9,9 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
       y: "25%",
       rotation: 5,
       opacity: 0,
-      stagger: { each: 0.05, total: 0.5 },
+      stagger: { each: 0.02, total: 0.25 },
       delay: 0.5,
-      duration: 1,
+      duration: 0.5,
       ease: "back.out(1.7)"
     });
   });
