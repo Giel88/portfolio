f1da288e502afa4702a75b76216c269650b760d9
Giel88 <giel88@gmail.com>
2025-09-15 15:31:22 +0200

    Update page-transition.js

--- page-transition.js
+++ page-transition.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
   gsap.fromTo(
     overlay,
     { opacity: 1 },
-    { opacity: 0, duration: 0.5, delay: 0.25 }
+    { opacity: 0, duration: 0.5, delay: 0.5 }
   );
 
   // Flag om dubbele navigaties te voorkomen
