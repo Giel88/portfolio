8311a8491243d2fc103981c8fac43b576a5145d2
Giel88 <giel88@gmail.com>
2025-09-15 10:25:15 +0200

    Create reinitialiseWebflow.js

---
+++ reinitialiseWebflow.js
@@ -0,0 +1,15 @@
+function reinitialiseWebflow(data) {
+  let parser = new DOMParser();
+  let dom = parser.parseFromString(data.next.html, 'text/html');
+  let webflowPageId = $(dom).find('html').attr('data-wf-page');
+
+  $('html').attr('data-wf-page', webflowPageId);
+
+  window.Webflow && window.Webflow.destroy();
+  window.Webflow && window.Webflow.ready();
+  
+  // OPTIONAL - E-commerce Reinitialisation
+  window.Webflow && Webflow.require('commerce').init({ siteId: "your-site-id-here"})
+
+  window.Webflow && window.Webflow.require('ix2').init();
+}
