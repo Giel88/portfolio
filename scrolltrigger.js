777bfe1888103b202cd740cdca77dc334f9b5311
Giel88 <giel88@gmail.com>
2025-09-15 19:51:35 +0200

    Create scrolltrigger.js

---
+++ scrolltrigger.js
@@ -0,0 +1,71 @@
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
+function textAndParagraphReveal() {
+  const headings = document.querySelectorAll('[data-content-reveal="heading"]');
+  headings.forEach(heading => {
+    const split = new SplitText(heading, { type: "chars" });
+    gsap.from(split.chars, {
+      y: "25%",
+      rotation: 5,
+      opacity: 0,
+      stagger: { each: 0.05, total: 0.5 },
+      delay: 0.5,
+      duration: 1,
+      ease: "back.out(1.7)"
+    });
+  });
+
+  const paragraphs = document.querySelectorAll('[data-content-reveal="paragraph"]');
+  paragraphs.forEach(paragraph => {
+    const split = new SplitText(paragraph, { type: "lines" });
+    gsap.from(split.lines, {
+      y: "25%",
+      opacity: 0,
+      stagger: { each: 0.05, total: 0.5 },
+      duration: 1,
+      ease: "back.out(1.7)",
+      scrollTrigger: {
+        trigger: paragraph,
+        start: "top bottom",
+        end: "top 70%",
+        toggleActions: "none play none none"
+      }
+    });
+  });
+
+  const lists = document.querySelectorAll('[data-content-reveal="list"]');
+  lists.forEach(list => {
+    gsap.from(list, {
+      y: "25%",
+      opacity: 0,
+      duration: 1,
+      ease: "back.out(1.7)",
+      scrollTrigger: {
+        trigger: list,
+        start: "top bottom",
+        end: "top 70%",
+        toggleActions: "none play none none"
+      }
+    });
+  });
+
+  const images = document.querySelectorAll('[data-content-reveal="image"]');
+  images.forEach(image => {
+    gsap.from(image, {
+      y: "10%",
+      opacity: 0,
+      duration: 1,
+      ease: "back.out(1.7)",
+      scrollTrigger: {
+        trigger: image,
+        start: "top bottom",
+        end: "top 70%",
+        toggleActions: "none play none none"
+      }
+    });
+  });
+}
+
+window.addEventListener('load', () => {
+  textAndParagraphReveal();
+});
