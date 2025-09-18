918a19456c432894919dbc3ccce268e2f9cc3aea
Giel88 <giel88@gmail.com>
2025-09-18 12:21:56 +0200

    Update barba.js

--- barba.js
+++ barba.js
@@ -9,6 +9,16 @@ function resetWebflow(data) {
   window.Webflow && window.Webflow.require("ix3").init();  
 }
 
+function autoplayVideos(container) {
+  const videos = container.querySelectorAll('video[autoplay]');
+  videos.forEach(video => {
+    video.pause();
+    video.currentTime = 0;
+    video.muted = true; // nodig voor autoplay op de meeste browsers
+    video.play().catch(e => console.log('Video autoplay blocked', e));
+  });
+}
+
 barba.init({
   transitions: [
     {
@@ -66,6 +76,9 @@ barba.init({
         requestAnimationFrame(() => {
           // Init ScrollTrigger animaties
           scrollReveal(data.next.container);
+          
+          // Init autoplay Videos
+          autoplayVideos(data.next.container);
       
           // Init horizontal loop
           const scrollContainer = data.next.container.querySelector(".scroll-container");
