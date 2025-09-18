0eba2235370adb6bf793d69a7b589de6ecfca530
Giel88 <giel88@gmail.com>
2025-09-18 11:39:20 +0200

    Update barba.js

--- barba.js
+++ barba.js
@@ -6,6 +6,7 @@ function resetWebflow(data) {
   window.Webflow && window.Webflow.destroy();
   window.Webflow && window.Webflow.ready();
   window.Webflow && window.Webflow.require("ix2").init();
+  window.Webflow && window.Webflow.require("ix3").init();  
 }
 
 barba.init({
