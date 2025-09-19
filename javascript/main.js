63747e38df3226a8a5839b744640327423bda96e
Giel88 <giel88@gmail.com>
2025-09-19 12:49:29 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,28 @@
+// ================================
+// Imports
+// ================================
 import { appState, initMouseTracking, initTicker } from './main-state.js';
 import { initCursor } from './cursor.js';
 import { initHoverAnimations, initCaseHover } from './hover.js';
 import { initScrollText } from './scroll.js';
 import { initBarba } from './barba.js';
 
+// ================================
+// Init basis state
+// ================================
 initMouseTracking();
 initTicker();
 initCursor();
+
+// ================================
+// Init scroll text op eerste page load
+// ================================
+const initialScrollContainer = document.querySelector(".scroll-container");
+if (initialScrollContainer) {
+  initScrollText(initialScrollContainer);
+}
+
+// ================================
+// Init Barba (zorgt voor page transitions en her-init van hover, scroll etc.)
+// ================================
 initBarba();
