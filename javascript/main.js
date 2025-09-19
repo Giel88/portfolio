4c7ed210d7554741f310c66a0e48b4c9c2160346
Giel88 <giel88@gmail.com>
2025-09-19 12:45:43 +0200

    Create main.js

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+import { appState, initMouseTracking, initTicker } from './main-state.js';
+import { initCursor } from './cursor.js';
+import { initHoverAnimations, initCaseHover } from './hover.js';
+import { initScrollText } from './scroll.js';
+import { initBarba } from './barba.js';
+
+initMouseTracking();
+initTicker();
+initCursor();
+initBarba();
