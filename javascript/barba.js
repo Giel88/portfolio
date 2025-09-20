4324e06ca3bf223646db346f044e4a8bdf58ba15
Giel88 <giel88@gmail.com>
2025-09-20 10:42:21 +0200

    Update barba.js

--- javascript/barba.js
+++ javascript/barba.js
@@ -42,8 +42,9 @@ export function initBarba() {
         async leave(data) {
           const triggerLink = data.trigger;
           const bgColor = triggerLink?.dataset?.caseColor || getComputedStyle(document.body).getPropertyValue('--bg');
-          gsap.to(document.body, { backgroundColor: bgColor, duration: 1, ease: "power1.inOut" });
           if (window.resetToDot) window.resetToDot();
+          
+          gsap.to(document.body, { backgroundColor: bgColor, duration: 1, ease: "power1.inOut" });
           await gsap.to(data.current.container, { autoAlpha: 0, duration: 1 });
         },
         enter(data) {
