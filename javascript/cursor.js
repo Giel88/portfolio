59fc68d63f388f5f9e028f68f2b751d641de7735
Giel88 <giel88@gmail.com>
2025-09-30 15:43:43 +0200

    Update cursor.js

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -35,7 +35,7 @@ export function initCursor() {
     if (!appState.hasMoved) {
       // Eerste muisbeweging: cursor direct positioneren, achtergrond vloeiend
       gsap.set(cursor, { x: e.clientX, y: e.clientY });
-      gsap.to(cursorbg, { opacity: 1, duration: 0.3, ease: "power2.out" });
+      gsap.set(cursorbg, { opacity: 1, duration: 0.3, ease: "power2.out" });
       appState.hasMoved = true;
       return; // geen animatie bij cursor zelf
     }
